5c265393ec20889e2e6782c8c36e28ea3be9a0c4
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-06-27 19:01:08 +0500

    Initial Setup

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let express = require("express");
-
 let app = express();
+let cookieParser = require("cookie-parser")
+let path = require("path")
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"public")))
+app.set('view engine','ejs')
 app.get("/", (req, res) => {
   res.send("Work");
 });
